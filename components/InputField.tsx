29c90089018e2250313be832f628e47723312880
EasinTanvir <easintanvir102@gmail.com>
2026-02-07 01:26:58 +0600

    refactor

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -23,7 +23,7 @@ const InputField = ({
           <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
         )}
         <TextInput
-          className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+          className={`rounded-full placeholder:text-primary-800 p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
           secureTextEntry={secureTextEntry}
           {...props}
         />
