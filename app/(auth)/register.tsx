1a743ba62a0fb45a87b288278c96894f61ac152e
EasinTanvir <easintanvir102@gmail.com>
2026-02-07 01:09:30 +0600

    added register page

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,11 +1,99 @@
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 const Register = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = () => {
+    // Handle sign-up logic here
+  };
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <ScrollView
+          className="bg-white flex-1"
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ paddingBottom: 140 }}
+        >
+          <View className="bg-white flex-1">
+            <View className="relative w-full h-[250px]">
+              <Image
+                className="z-0 w-full h-[250px]"
+                source={images.signUpCar}
+                alt="signUpCar"
+              />
+              <Text className="absolute text-black font-jakarta-bold bottom-4 left-5 text-2xl font-semibold">
+                Create Your Account
+              </Text>
+            </View>
+
+            <View className="pt-28 px-4">
+              <InputField
+                label="Name"
+                placeholder="Enter name"
+                icon={icons.person}
+                value={form.name}
+                onChangeText={(value) => setForm({ ...form, name: value })}
+              />
+              <InputField
+                label="Email"
+                placeholder="Enter email"
+                icon={icons.email}
+                textContentType="emailAddress"
+                value={form.email}
+                onChangeText={(value) => setForm({ ...form, email: value })}
+              />
+              <InputField
+                label="Password"
+                placeholder="Enter password"
+                icon={icons.lock}
+                secureTextEntry={true}
+                textContentType="password"
+                value={form.password}
+                onChangeText={(value) => setForm({ ...form, password: value })}
+              />
+            </View>
+          </View>
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+          <Link
+            href="/(auth)/login"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Already have an account?{" "}
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
