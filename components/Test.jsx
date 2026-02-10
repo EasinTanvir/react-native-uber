56b72adddb795790a93ba1c2f986c2243b770f02
EasinTanvir <easintanvir102@gmail.com>
2026-02-10 18:46:45 +0600

    refactor

--- components/Test.jsx
+++
@@ -1,9 +0,0 @@
-// import _ from "lodash";
-// import uniqueId from "lodash/uniqueId";
-
-// const Profile = () => {
-//   return <div className="min-h-screen">Profile</div>;
-// };
-
-// export default Profile;
-// you can check the coas of your import statemnet
